552c5fd1a0a94ce7c0fea07570bb762113cc1738
damiBass <borgognoni.damian@gmail.com>
2023-11-24 19:34:35 -0300

    mail

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,57 +1,40 @@
-window.addEventListener("load", function () {
-  /* =============================================================================
-        -----------------------------  Contact Validation   -----------------------------
-        ============================================================================= */
-
+document.addEventListener("DOMContentLoaded", function () {
   document
     .getElementById("contact-form")
     .addEventListener("submit", function (e) {
       e.preventDefault();
 
-      // Crear un objeto FormData con los datos del formulario
       var formData = new FormData(this);
 
-      // Crear una instancia de XMLHttpRequest
       var xhr = new XMLHttpRequest();
-
-      // Configurar la solicitud
       xhr.open("POST", "contact.php", true);
 
-      // Configurar el manejo de la respuesta
       xhr.onreadystatechange = function () {
-        console.log(xhr.responseText);
         if (xhr.readyState == 4) {
           if (xhr.status == 200) {
-            // Manejar la respuesta del servidor
-            if (
-              xhr.responseText.includes(
-                "El formulario de contacto se envió con éxito."
-              )
-            ) {
-              // Éxito, puedes realizar las acciones necesarias
-              console.log("Formulario enviado con éxito");
+            var data = JSON.parse(xhr.responseText);
+            var messageAlert = "alert-" + data.type;
+            var messageText = data.message;
+
+            var alertBox =
+              '<div class="alert ' +
+              messageAlert +
+              ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+              messageText +
+              "</div>";
+
+            if (messageAlert && messageText) {
               document
                 .getElementById("contact-form")
-                .querySelector(".messages").innerHTML = xhr.responseText;
+                .querySelector(".messages").innerHTML = alertBox;
               document.getElementById("contact-form").reset();
-
-              // Recargar la página y volver al inicio después de un breve retraso (por ejemplo, 1 segundo)
-              setTimeout(function () {
-                location.reload();
-                // O puedes usar location.href = "#inicio"; si tienes un ancla con el id "inicio"
-              }, 1000);
-            } else {
-              // La respuesta no coincide con el éxito esperado
-              console.error("Error: La respuesta no es la esperada.");
             }
           } else {
-            // Manejar errores de HTTP
             console.error("Error HTTP:", xhr.status);
           }
         }
       };
 
-      // Enviar la solicitud con los datos del formulario
       xhr.send(formData);
     });
 });
--- contact.php
+++ contact.php
@@ -1,18 +1,13 @@
 <?php
 
-// Configuración
-$from = 'Demo contact form <borgognoni.damian@gmail.com>';
-$sendTo = 'Test contact form <borgognoni.damian@gmail.com>';
+$sendTo = 'borgognoni.damian@gmail.com';
 $subject = 'New message from contact form';
-$fields = array('name' => 'Name', 'subject' => 'Subject', 'email' => 'Email', 'message' => 'Message');
+$fields = array('name' => 'Name', 'email' => 'Email', 'subject' => 'Subject', 'message' => 'Message');
 $okMessage = 'Contact form successfully submitted. Thank you, we will get back to you soon!';
 $errorMessage = 'There was an error while submitting the form. Please try again later.';
 
-// Procesar el formulario
 try {
-    // Verificar si es una solicitud POST
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Recopilar los datos del formulario
         $emailText = "You have a new message from the contact form\n=============================\n";
 
         foreach ($_POST as $key => $value) {
@@ -21,15 +16,13 @@ try {
             }
         }
 
-        // Configurar los encabezados del correo
         $headers = array(
             'Content-Type: text/plain; charset="UTF-8";',
-            'From: ' . $from,
-            'Reply-To: ' . $from,
-            'Return-Path: ' . $from,
+            'From: ' . $sendTo,
+            'Reply-To: ' . $sendTo,
+            'Return-Path: ' . $sendTo,
         );
 
-        // Enviar el correo
         if (mail($sendTo, $subject, $emailText, implode("\n", $headers))) {
             $responseArray = array('type' => 'success', 'message' => $okMessage);
         } else {
@@ -42,13 +35,6 @@ try {
     $responseArray = array('type' => 'danger', 'message' => $errorMessage, 'error' => $e->getMessage());
 }
 
-// Responder a la solicitud Ajax
-if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-    header('Content-Type: application/json');
-    echo json_encode($responseArray);
-} else {
-    // No imprimir mensajes en la página web
-    // echo $responseArray['message'];
-}
+header('Content-Type: application/json');
+echo json_encode($responseArray);
 ?>
-
